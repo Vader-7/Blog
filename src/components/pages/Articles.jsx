29546ad72a634185555efb4686779a594048d01c
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-12-28 21:47:31 -0300

    update fetch

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -15,28 +15,31 @@ export const Articles = () => {
         method: 'GET',
       });
       const data = await response.json();
-      setArticles(data);
+      setArticles(data.articles);
     } catch (error) {
       console.error(error);
     }
   };
-
   return (
     <>
-      {articles.map(article => (
-        <article key={article._id} className="article-item">
-          <div className="mask">
-            <img src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" alt="" />
-          </div>
-          <div className="data">
-            <h3 className="title">{article.title}</h3>
-            <p className="description">{article.content}</p>
+      {articles.length > 0 ? (
+        articles.map(article => (
+          <article key={article._id} className="article-item">
+            <div className="mask">
+              <img src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" alt="" />
+            </div>
+            <div className="data">
+              <h3 className="title">{article.title}</h3>
+              <p className="description">{article.content}</p>
 
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-          </div>
-        </article>
-      ))}
+              <button className="edit">Editar</button>
+              <button className="delete">Borrar</button>
+            </div>
+          </article>
+        ))
+      ) : (
+        <p>Loading articles...</p>
+      )}
     </>
   );
 };
