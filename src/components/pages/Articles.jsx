db3b5b5a8b49d5648350780cd47947b3200b506e
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-12-28 21:33:53 -0300

    fetch articles

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -1,46 +1,42 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 
 export const Articles = () => {
-    return (
-        <>
-            <article className="article-item">
-                <div className='mask'>
-                    <img src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" alt="" />
-                </div>
-                <div className="data">
-                    <h3 className="title">Desarrollo web</h3>
-                    <p className="description">tylerxd.es</p>
+  const [articles, setArticles] = useState([]);
 
-                    <button className="edit">Editar</button>
-                    <button className="delete">Borrar</button>
-                </div>
-            </article>
+  useEffect(() => {
+    getData();
+  }, []);
 
-            <article className="article-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">xd.es</p>
+  const getData = async () => {
+    const url = 'http://localhost:3900/api/articles';
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+      });
+      const data = await response.json();
+      setArticles(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+  return (
+    <>
+      {articles.map(article => (
+        <article key={article._id} className="article-item">
+          <div className="mask">
+            <img src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" alt="" />
+          </div>
+          <div className="data">
+            <h3 className="title">{article.title}</h3>
+            <p className="description">{article.content}</p>
 
-            <article className="article-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">tyleresweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="article-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">asweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-        </>
-    )
-}
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+          </div>
+        </article>
+      ))}
+    </>
+  );
+};
