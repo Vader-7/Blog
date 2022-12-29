9aadfa71f72a97cdfeb24fed4e50bfe0f2738856
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-12-28 21:52:25 -0300

    url update

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -2,44 +2,44 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 export const Articles = () => {
-  const [articles, setArticles] = useState([]);
+    const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    getData();
-  }, []);
+    useEffect(() => {
+        getData();
+    }, []);
 
-  const getData = async () => {
-    const url = 'http://localhost:3900/api/articles';
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-      });
-      const data = await response.json();
-      setArticles(data.articles);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  return (
-    <>
-      {articles.length > 0 ? (
-        articles.map(article => (
-          <article key={article._id} className="article-item">
-            <div className="mask">
-              <img src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" alt="" />
-            </div>
-            <div className="data">
-              <h3 className="title">{article.title}</h3>
-              <p className="description">{article.content}</p>
+    const getData = async () => {
+        const url = Global.url + 'articles';
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+            });
+            const data = await response.json();
+            setArticles(data.articles);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return (
+        <>
+            {articles.length > 0 ? (
+                articles.map(article => (
+                    <article key={article._id} className="article-item">
+                        <div className="mask">
+                            <img src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" alt="" />
+                        </div>
+                        <div className="data">
+                            <h3 className="title">{article.title}</h3>
+                            <p className="description">{article.content}</p>
 
-              <button className="edit">Editar</button>
-              <button className="delete">Borrar</button>
-            </div>
-          </article>
-        ))
-      ) : (
-        <p>Loading articles...</p>
-      )}
-    </>
-  );
+                            <button className="edit">Editar</button>
+                            <button className="delete">Borrar</button>
+                        </div>
+                    </article>
+                ))
+            ) : (
+                <p>Loading articles...</p>
+            )}
+        </>
+    );
 };
